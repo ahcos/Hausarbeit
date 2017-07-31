9f39751f55249aa596e3b957432238c1b1feea8a
FME <MarianEilers@gmx.de>
2017-07-31 23:55:04 +0200

    0.01 Begin

--- Eilers_4078322/src/t12/Application.java
+++ Eilers_4078322/src/t12/Application.java
@@ -41,7 +41,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		useGUIPhone();
-		// useConsolePhone();
+//		 useConsolePhone();
 	}
 
 	/**
---
+++ Eilers_4078322/src/t12/T12itp.java
@@ -0,0 +1,67 @@
+package t12;
+
+import t12.spinphone.T12Interpreter;
+
+public class T12itp implements T12Interpreter {
+
+	@Override
+	public String buttonPressed(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void generateLexicon(String pathToTexts, String lexFileDestination) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadLexicon(String lexFilePath) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getAlternative() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void learn(String newWord) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void asteriskButtonPressed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void numberSignButtonPressed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String delButtonPressed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void wordCompleted() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getAuthorName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
